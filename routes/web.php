e94a0ddbfefd79608059e653b46d02c53697041f
Subhan Raj <shubhanraj2002@gmail.com>
2023-11-16 23:27:14 +0530

    Add ViewsController and index view route.

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ViewsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Views Routes
+
+Route::controller(ViewsController::class)->prefix('/')->group(function(){
+    Route::get('/', 'index')->name('index');
 });
