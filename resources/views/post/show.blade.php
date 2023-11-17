86f119aba84ae0eddf39b6c35c3dc5f99445dea0
Subhan Raj <shubhanraj2002@gmail.com>
2023-11-17 20:41:41 +0530

    Add layout and update post creation route.

---
+++ resources/views/post/show.blade.php
@@ -0,0 +1,5 @@
+<x-layout>
+    <x-slot:title>
+        An Exciting Post
+    </x-slot:title>
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,7 @@ Route::controller(ViewsController::class)->prefix('/')->group(function(){
 Route::controller(PostController::class)->prefix('/')->group(function(){
     Route::get('/posts', 'index')->name('posts.index');
     Route::get('/posts/create-new-post', 'create')->name('posts.create');
-    Route::post('/posts', 'store')->name('posts.store');
+    Route::post('/posts/create-new-post', 'store')->name('posts.store');
     Route::get('/posts/{post}', 'show')->name('posts.show');
     Route::get('/posts/{post}/edit', 'edit')->name('posts.edit');
     Route::put('/posts/{post}', 'update')->name('posts.update');
