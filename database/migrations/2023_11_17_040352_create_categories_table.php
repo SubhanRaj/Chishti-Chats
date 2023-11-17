87b24450745c24e74410f51227f2fce5f62fad4a
Subhan Raj <shubhanraj2002@gmail.com>
2023-11-17 09:41:26 +0530

    Add migration for creating categories table.

---
+++ database/migrations/2023_11_17_040352_create_categories_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            // categoryID, primary key, int, max 10 digits
+            $table->id('categoryID')->primary();
+            // categoryName, string, max 255 characters, unique
+            $table->string('categoryName', 255)->unique();
+            // created_at, updated_at
+            $table->timestamps();
+            // deleted_at
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('categories');
+    }
+};
