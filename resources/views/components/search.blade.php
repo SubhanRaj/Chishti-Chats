d0790c1583cd9ec09f26fd3f7421b21e97202fb5
Subhan Raj <shubhanraj2002@gmail.com>
2023-11-17 19:03:12 +0530

    New Routes & Pages

---
+++ resources/views/components/search.blade.php
@@ -0,0 +1,44 @@
+<div class="header__search">
+    <form action="#">
+        <label>
+            <i class="icon-Search js-header-search-btn-open"></i>
+            <input type="search" placeholder="Search" class="form-control">
+        </label>
+    </form>
+    <div class="header__search-close js-header-search-btn-close"><i class="icon-Cancel"></i></div>
+    <div class="header__search-btn" data-dropdown-btn="search">
+        Options<i class="icon-Arrow_Below"></i>
+    </div>
+    <div class="dropdown dropdown--design-01" data-dropdown-list="search">
+        <ul>
+            <li>
+                <label>
+                    <label class="custom-checkbox">
+                        <input type="checkbox" checked="checked">
+                        <i></i>
+                    </label>Search Titles Only
+                </label>
+
+            </li>
+            <li>
+                <label>
+                    <label class="custom-checkbox">
+                        <input type="checkbox">
+                        <i></i>
+                    </label>Search Categories Only
+                </label>
+
+            </li>
+            <li>
+                <label>
+                    <label class="custom-checkbox">
+                        <input type="checkbox" >
+                        <i></i>
+                    </label>Search Every Where
+                </label>
+
+            </li>
+
+        </ul>
+    </div>
+</div>
