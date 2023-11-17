492904c753639dc02da83a88aa2bc7d9577bc157
Subhan Raj <shubhanraj2002@gmail.com>
2023-11-17 09:39:07 +0530

    Add columns to posts table.

--- database/migrations/2023_11_16_200349_create_posts_table.php
+++ database/migrations/2023_11_16_200349_create_posts_table.php
@@ -12,8 +12,24 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('posts', function (Blueprint $table) {
-            $table->id();
+            // postID : primary key, int, max 10 digits
+            $table->id('postID')->primary();
+            // userID: foreign key, int, max 3 digits
+            $table->foreignId('userID')->constrained('users');
+            // categoryID: foreign key, int, max 10 digits
+            $table->foreignId('categoryID')->constrained('categories');
+            // title: string, max 255 characters
+            $table->string('title', 255);
+            // content: text
+            $table->text('content');
+            // filePath: string, max 255 characters
+            $table->string('filePath', 255);
+            // tags, csv
+            $table->string('tags', 255);
+            // created_at, updated_at
             $table->timestamps();
+            // deleted_at
+            $table->softDeletes();
         });
     }
 
