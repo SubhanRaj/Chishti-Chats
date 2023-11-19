d82457d3cfb231bab364737a8f22f46e0c9498f6
Subhan Raj <shubhanraj2002@gmail.com>
2023-11-19 12:06:37 +0530

    Update breadcrumb.blade.php file

--- resources/views/components/breadcrumb.blade.php
+++ resources/views/components/breadcrumb.blade.php
@@ -1,19 +1,28 @@
 <!--================Forum Breadcrumb Area =================-->
+@props(['title', 'url'])
+
 <section class="page_breadcrumb">
     <div class="container-fluid pl-60 pr-60">
         <div class="row">
             <div class="col-sm-7">
                 <nav aria-label="breadcrumb">
                     <ol class="breadcrumb">
-                        <li class="breadcrumb-item"><a href="{{route('index')}}">Home</a></li>
-                        <li class="breadcrumb-item active" aria-current="page"><a href="#">All Posts</a></li>
+                        <li class="breadcrumb-item"><a href="{{ route('index') }}">Home</a></li>
+                        <li class="breadcrumb-item @if(request()->is($url)) active @endif" aria-current="page">
+                            @if(isset($url))
+                            <a href="{{ $url }}">{{ $title }}</a>
+                            @else
+                            {{ $title }}
+                            @endif
+                        </li>
                     </ol>
                 </nav>
             </div>
             <div class="col-sm-5">
-                <a href="#" class="date"><i class="icon_clock_alt"></i>Updated on March 03, 2020</a>
+                <a href="#" class="date"><i class="icon_clock_alt"></i> Updated on {{ date('d M, Y') }}</a>
             </div>
         </div>
     </div>
 </section>
+
 <!--================End Forum Breadcrumb Area =================-->
