4e90a8379e6be3286b164480507d44baf033e139
Subhan Raj <shubhanraj2002@gmail.com>
2023-11-21 00:39:45 +0530

    Update favicon and logo paths in header.blade.php

--- resources/views/admin/header.blade.php
+++ resources/views/admin/header.blade.php
@@ -11,12 +11,11 @@
     <meta http-equiv='Cache-Control' content='no-cache'>
     <!--favicon-->
 
-    <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/png" />
+    <link rel="icon" href="{{ asset('assets/img/favicon/favicon.ico') }}" type="image/png" />
 
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&amp;display=swap" rel="stylesheet">
 
-    <link href="{{ asset('admin-assets/assets/plugins/datatable/css/dataTables.bootstrap5.min.css') }}"
-        rel="stylesheet">
+    <link href="{{ asset('admin-assets/assets/plugins/datatable/css/dataTables.bootstrap5.min.css') }}" rel="stylesheet">
     <link href="{{ asset('admin-assets/assets/plugins/simplebar/css/simplebar.css') }}" rel="stylesheet" />
 
     <link href="{{ asset('admin-assets/assets/plugins/metismenu/css/metisMenu.min.css') }}" rel="stylesheet" />
@@ -43,10 +42,8 @@
         <div class="sidebar-wrapper" data-simplebar="true">
             <div class="sidebar-header">
                 <div class="d-flex justify-content-center align-items-center w-100">
-                    <img src="{{ asset('admin-assets/assets/images/icons/dashboard.png') }}" class="logo-icon logo-big"
-                        alt="logo icon">
-                    <img src="{{ asset('admin-assets/assets/images/icons/dashboard.png') }}"
-                        class="logo-icon d-none logo-sm" alt="logo icon">
+                    <img src="{{ asset('assets/img/logo/logo-full.png') }}" class="logo-icon logo-big" alt="logo icon">
+                    <img src="{{ asset('assets/img/logo/logo.png') }}" class="logo-icon d-none logo-sm" alt="logo icon">
                 </div>
 
                 <div class="toggle-icon ms-auto"><i class='bx bx-arrow-to-left'></i>
@@ -64,14 +61,15 @@
 
                 <li>
                     <a href="{{ route('admin.adminPostCategory') }}">
-                        <div class="parent-icon"><i class='bx bx-home-circle'></i>
+                        <div class="parent-icon"><i class="fa-solid fa-layer-group"></i>
                         </div>
                         <div class="menu-title">Category</div>
                     </a>
                 </li>
                 <a class="nav-link" href="{{ route('admin.showTag') }}">
-                    <div class="d-flex align-items-center"><span class="nav-link-icon"><span
-                                class="fas fa-tag"></span></span><span class="nav-link-text ps-1">Tag</span></div>
+                    <div class="parent-icon"><i class="fa-solid fa-tags"></i>
+                    </div>
+                    <div class="menu-title">Tags</div>
                 </a>
                 <li>
                     <a href="{{ route('admin.viewPosts') }}">
@@ -82,16 +80,16 @@
                 </li>
                 <li>
                     <a href="{{ route('admin.showComment') }}">
-                        <div class="parent-icon"><i class="fa-solid fa-blog"></i>
+                        <div class="parent-icon"><i class="fa-solid fa-comment"></i>
                         </div>
                         <div class="menu-title">Comments</div>
                     </a>
                 </li>
                 <li>
                     <a href="javascript:;" class="has-arrow">
-                        <div class="parent-icon"><i class="fa-solid fa-sitemap"></i>
+                        <div class="parent-icon"><i class="fa-brands fa-searchengin"></i>
                         </div>
-                        <div class="menu-title">Meta</div>
+                        <div class="menu-title">Meta Tags</div>
                     </a>
                     <ul>
                         <li> <a href="{{ route('admin.addMeta') }}"><i class="bx bx-right-arrow-alt"></i>Add
@@ -132,25 +130,18 @@
                     <div class="top-menu ms-auto">
                         <ul class="navbar-nav align-items-center">
                             <li class="nav-item">
-                                <a class="nav-link" href="#" data-bs-toggle="tooltip"
-                                    data-bs-title="Full Screen Mode" data-bs-placement="auto" onclick="fullScreen()"
-                                    id="fullscreen-btn"> <i class='bx bx-fullscreen' id="fullscreen-icon"></i>
+                                <a class="nav-link" href="#" data-bs-toggle="tooltip" data-bs-title="Full Screen Mode" data-bs-placement="auto" onclick="fullScreen()" id="fullscreen-btn"> <i class='bx bx-fullscreen' id="fullscreen-icon"></i>
                                 </a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link theme-change" href="#" id="theme-change-btn"
-                                    onclick="changeTheme()" data-bs-toggle="tooltip"
-                                    data-bs-title="Switch to dark mood" data-bs-placement="auto"> <i
-                                        class='bx bx-moon'></i>
+                                <a class="nav-link theme-change" href="#" id="theme-change-btn" onclick="changeTheme()" data-bs-toggle="tooltip" data-bs-title="Switch to dark mood" data-bs-placement="auto"> <i class='bx bx-moon'></i>
                                 </a>
                             </li>
                         </ul>
                     </div>
                     <div class="user-box dropdown">
-                        <a class="d-flex align-items-center nav-link dropdown-toggle dropdown-toggle-nocaret"
-                            href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            <img src="{{ asset('admin-assets/assets/images/icons/profile.png') }}" class="user-img"
-                                alt="user avatar">
+                        <a class="d-flex align-items-center nav-link dropdown-toggle dropdown-toggle-nocaret" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            <img src="{{ asset('admin-assets/assets/images/icons/profile.png') }}" class="user-img" alt="user avatar">
                             <div class="user-info ps-3">
                                 <p class="user-name mb-0">Chisti Chats</p>
                                 {{-- <p class="designattion mb-0">Digital Marketing Agency</p> --}}
@@ -158,8 +149,7 @@
                         </a>
                         <ul class="dropdown-menu dropdown-menu-end">
 
-                            <li><a class="dropdown-item" href="{{ route('admin.logout') }}"><i
-                                        class='bx bx-log-out-circle'></i><span>Logout</span></a>
+                            <li><a class="dropdown-item" href="{{ route('admin.logout') }}"><i class='bx bx-log-out-circle'></i><span>Logout</span></a>
                             </li>
                         </ul>
                     </div>
