c4392ddedd6eabb38443d8137dc4a7168b3c42fc
Subhan Raj <shubhanraj2002@gmail.com>
2023-11-22 01:20:27 +0530

    soethign

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -35,14 +35,13 @@
                         <aside class="l_widget forum_list">
                             <h3 class="wd_title">Categories</h3>
                             <ul class="navbar-nav">
-                                @for ($i = 0; $i < 5; $i++) 
-                                <li class="nav-item">
+                                @for ($i = 0; $i < 5; $i++) <li class="nav-item">
                                     <a href="#"><i class="icon_lightbulb_alt"></i>General</a>
-                                </li>
-                                @endfor
-                                <li class="active nav-item">
-                                    <a href="#"><i class=""></i>View all</a>
-                                </li>
+                                    </li>
+                                    @endfor
+                                    <li class="active nav-item">
+                                        <a href="#"><i class=""></i>View all</a>
+                                    </li>
                             </ul>
                         </aside>
                     </div>
@@ -78,28 +77,7 @@
                                 <ul class="nav right">
                                     <li>
                                         <div class="dropdown right_dir dropstart">
-                                            <button class="btn dropdown-toggle" type="button" data-bs-toggle="dropdown"
-                                                aria-haspopup="true" aria-expanded="false">
-                                                Tags <i class="arrow_carrot-down"></i>
-                                            </button>
-                                            <div class="dropdown-menu">
-                                                <h3 class="title">Filter by Tags</h3>
-                                                <form action="#" class="cate-search-form">
-                                                    <input type="text" placeholder="Search Tags">
-                                                </form>
-                                                <div class="all_users scroll">
-                                                    @for ($i = 0; $i < 5; $i++)
-                                                        <a class="dropdown-item" href="#"><span
-                                                                class="color red"></span> bugs</a>
-                                                    @endfor
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </li>
-                                    <li>
-                                        <div class="dropdown right_dir dropstart">
-                                            <button class="btn dropdown-toggle" type="button" data-bs-toggle="dropdown"
-                                                aria-haspopup="true" aria-expanded="false">
+                                            <button class="btn dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                                 Sort <i class="arrow_carrot-down"></i>
                                             </button>
                                             <div class="dropdown-menu">
@@ -121,34 +99,36 @@
                             <div class="forum_body">
                                 <ul class="navbar-nav topic_list">
                                     @foreach ($posts as $post)
-                                        <li>
-                                            <div class="media">
-                                                <div class="d-flex">
-                                                    <img class="rounded-circle"
-                                                        src="{{ asset('assets/img/forum/f-user-1.png') }}"
-                                                        alt="">
-                                                </div>
-                                                <div class="media-body">
-                                                    <div class="t_title">
-                                                        <a href="{{ route('posts.show', $post->slug) }}">
-                                                            <h4>
-                                                                {{ $post->title }}
-                                                            </h4>
-                                                        </a>
-                                                    </div>
-                                                    <a href="#">
-                                                        <h6>
-                                                            <img src="{{ asset('assets/img/svg/hashtag.svg') }}"
-                                                                alt=""> General
-                                                        </h6>
-                                                    </a>
-                                                    <h6>
-                                                        <i class="icon_clock_alt"></i>
-                                                        {{ $post->created_at->diffForHumans() }}
-                                                    </h6>
-                                                </div>
+                                    <li>
+                                        <div class="media">
+                                            <div class="d-flex">
+                                                @php
+                                                $user_first_letter = firstLetter($user_id);
+                                                @endphp
+                                                @if ($user_first_letter != false)
+                                                {{ $user_first_letter }}
+                                                @endif
                                             </div>
-                                        </li>
+                                            <div class="media-body">
+                                                <div class="t_title">
+                                                    <a href="{{ route('posts.show', $post->slug) }}">
+                                                        <h4>
+                                                            {{ $post->title }}
+                                                        </h4>
+                                                    </a>
+                                                </div>
+                                                <a href="#">
+                                                    <h6>
+                                                        <img src="{{ asset('assets/img/svg/hashtag.svg') }}" alt=""> General
+                                                    </h6>
+                                                </a>
+                                                <h6>
+                                                    <i class="icon_clock_alt"></i>
+                                                    {{ $post->created_at->diffForHumans() }}
+                                                </h6>
+                                            </div>
+                                        </div>
+                                    </li>
                                     @endforeach
                                 </ul>
                             </div>
@@ -162,15 +142,13 @@
                             <div class="col-lg-8">
                                 <nav aria-label="Page navigation example">
                                     <ul class="pagination justify-content-center">
-                                        <li class="page-item"><a class="page-link" href="#"><i
-                                                    class="arrow_carrot-left"></i> Previous</a></li>
+                                        <li class="page-item"><a class="page-link" href="#"><i class="arrow_carrot-left"></i> Previous</a></li>
                                         <li class="page-item active"><a class="page-link" href="#">1</a></li>
                                         <li class="page-item"><a class="page-link" href="#">2</a></li>
                                         <li class="page-item"><a class="page-link" href="#">3</a></li>
                                         <li class="page-item"><a class="page-link" href="#">...</a></li>
                                         <li class="page-item"><a class="page-link" href="#">21</a></li>
-                                        <li class="page-item"><a class="page-link" href="#">Next <i
-                                                    class="arrow_carrot-right"></i></a></li>
+                                        <li class="page-item"><a class="page-link" href="#">Next <i class="arrow_carrot-right"></i></a></li>
                                     </ul>
                                 </nav>
                             </div>
@@ -180,10 +158,8 @@
                 <div class="col-xl-3 col-lg-4">
                     <div class="right_side_forum">
                         <aside class="r_widget qustion_wd">
-                            <button class="btn" type="button"
-                                onclick="window.location.href='{{ route('profile') }}'">
-                                <img src="{{ asset('assets/img/forum/helpful-user/question-1.png') }}"
-                                    alt="">Ask Question <i class="arrow_carrot-right"></i>
+                            <button class="btn" type="button" onclick="window.location.href='{{ route('profile') }}'">
+                                <img src="{{ asset('assets/img/forum/helpful-user/question-1.png') }}" alt="">Ask Question <i class="arrow_carrot-right"></i>
                             </button>
                         </aside>
                     </div>
